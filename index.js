5351e8aa3a08cbe5bc2de6024605b6de00c87894
mdamanshaikh2004 <amanshaikh2k22@gmail.com>
2026-04-24 20:47:43 +0530

    fixed port issue

--- index.js
+++ index.js
@@ -26,8 +26,10 @@ let posts = [{
 
 }]
 
-app.listen(port, () => {
-    console.log("listening to part : 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 app.get("/", (req, res) => {
     res.send("Quora Posts API is running ");
